d2f32420f4652f6fdbe14f09cc322c593292ad84
pappusrivastav <144468141+pappusrivastav@users.noreply.github.com>
2023-11-02 13:08:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
+let bandNames = ['The Beatles', 'Aerosmith', 'The Rolling Stones', 'Queen', 'Led Zeppelin', 'Red Hot Chili Peppers', 'The Who', 'Anthrax'];
+
+// Function to sort band names without articles
+function sortBandNames(bandNames) {
+    const articles = /^(a |an |the )/i;
+    return bandNames.sort((a, b) => {
+        const aName = a.replace(articles, '');
+        const bName = b.replace(articles, '');
+        return aName.localeCompare(bName);
+    });
+}
+
+// Sorting the band names
+let sortedBandNames = sortBandNames(bandNames);
+
+// Displaying the sorted band names
+let ul = document.getElementById('band');
+sortedBandNames.forEach(name => {
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(name));
+    ul.appendChild(li);
+});
+
 
